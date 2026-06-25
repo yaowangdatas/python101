a272f8b05383fde058ebfb222e9b3fcfda78776e
YaoWang <ywangyz@gmail.com>
2026-06-25 14:15:52 -0400

    This is my first python test.

---
+++ hello.py
@@ -0,0 +1,108 @@
+# print("Hello World!")
+# print("My name is Yao.")
+
+# # ctrl+/ is how we make a comment
+# # this is a string example
+# learner = "optimus Prima"
+# print(learner)
+
+# # this is a number/integer example
+# ideal_number_of_pets = 2
+# print(ideal_number_of_pets)
+
+# # This a float/decimal example
+# how_much_is_a_banana = 9.99
+# print(how_much_is_a_banana)
+
+# # This is an example of a Boolean True/False
+# is_our_pets_vaccinated = True
+# print(is_our_pets_vaccinated)
+
+# print(type(ideal_number_of_pets))
+# print(type(how_much_is_a_banana))
+# print(type(learner))
+# print(type(is_our_pets_vaccinated))
+
+# is_it_a_number = 333
+# is_this_a_number = "333"
+
+# print(type(is_it_a_number))
+# print(type(is_this_a_number))
+
+# statement = "The number of pets I want is 2 dogs."
+
+# puppy_name = "Buddy"
+# what_is_buddy_age = 3
+# how_old_is_colton = 21.5
+# is_line_36_true = True
+# zipcode = '100001'
+
+# # print(statement)
+# print(statement)
+# print(statement)
+
+# print(5+3)
+# print(5*3)
+# print(5-3)
+# print(5/3)
+
+# print(10/5)
+# print(7/3)
+# print(12/4)
+# print(99/8)
+
+# Example of modulo
+# print(10%3)
+# print(10/3)
+# print(10%5)
+
+# # show an example of an exponent "power to the 5th"
+# print(2**5)
+# print(2**2)
+# print(32**12)
+
+# find the area of a ectangle
+# length = int(input("What is the length?"))
+# width = int(input("What is the width"))
+# area = length * width
+# print(f"The area of rectangle is {area}.")
+
+# # find the tax amount
+# price = 9
+# tax = price * 0.08
+# print(tax)
+
+# # find the average
+# avg = (10+10 +10)/3
+# print(avg)
+
+# # Ask the user their favorite color
+# fav_color = input("What is your favorite color?")
+
+# print(f"Your favorite color is {fav_color}")
+
+# This is a comment to test multiple input
+# print('This', 'is', 'a', 'sample.')
+# print(12, 24, -2, sep=':')
+# print('but', 'not', 'including', sep='**')
+# print('but', 'not', 'including', sep='')
+
+# create a receipt
+customerName = input("Enter customer's name: ")
+itemPrice = float(input("What is the price of the item?"))
+quantity = int(input("Enter quantity:"))
+totalCost = itemPrice*quantity
+roundedCost = round(totalCost,2)
+
+print("Receipt")
+print("-------------------")
+# customer name
+print(f"Customer: {customerName}")
+# the price of what they purchased
+print(f"Item price: ${itemPrice}")
+# the quantity of the product they purchase
+print(f"Quantity: {quantity}")
+# the total cost
+print(f"Total Cost: ${roundedCost}")
+
+
